421efb3dd951e11ba4ef181c34d9a0b47d6f3e8b
yestarani <yestaranikxworkz@gmail.com>
2024-07-27 00:28:37 +0530

    third commit

---
+++ Voter.java
@@ -0,0 +1,18 @@
+class Voter
+{
+static void age(int age)
+{
+System.out.println("start main in Voter");
+System.out.println("age:"+age);
+
+if(age>=18)
+{
+System.out.println("age is valid");
+}
+else
+{
+System.out.println("age is invalid");
+}
+System.out.println("end main in Voter");
+}
+}
